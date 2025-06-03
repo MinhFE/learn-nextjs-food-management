3a4111c8d5c485af8ff63461218baee2721ceeac
Phan Dinh Minh <phandinhminh16082001@gmail.com>
2025-06-04 00:52:48 +0700

    short polling technique

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     'next',
     'next/core-web-vitals',
     'plugin:prettier/recommended', // Kết hợp với Prettier
+    'plugin:@tanstack/query/recommended',
   ],
   rules: {
     'prettier/prettier': ['error', { singleQuote: true }],
